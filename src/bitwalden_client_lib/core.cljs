2a00d7570572a7893f57827df7c906d623c09e3c
Friar Tuck <f.tuck@gmx.com>
2017-10-31 03:52:37 +0000

    Sign JSON RPC request by default + moving fns around.

--- src/bitwalden_client_lib/core.cljs
+++ src/bitwalden_client_lib/core.cljs
@@ -12,6 +12,18 @@
 
 (def profile-namespace "bw.profile")
 
+; test params
+; (def node "http://localhost:8923")
+; (def keypair (keypair-from-seed-b58 "H33xgBQj5jTU6bKC5iw6B9docquvNpDeKoSSWkCpcU58"))
+; (def pubkey (public-key-b58-from-keypair keypair))
+
+; -- utils --- ;
+
+(defn with-timestamp [params]
+  (assoc params :t (.getTime (js/Date.))))
+
+; --- data format utils --- ;
+
 (defn hexenate [b]
   (.join (.map (js/Array.from (.slice b)) #(.slice (str "0" (.toString (bit-and % 0xff) 16)) -2)) ""))
 
@@ -28,17 +40,6 @@
     (.set n b (.-length a))
     n))
 
-(defn <api [method uri & [opts]]
-  (let [c (chan)]
-    (apply ({:get GET :post POST} method) [uri (assoc opts :handler #(put! c [:ok %]) :error-handler #(put! c [:error %]))])
-    c))
-
-(defn <json-rpc [node method params]
-  (go (let [[code response] (<! (<api :post (str node "/bw/rpc") {:params {"jsonrpc" "2.0" "method" method "id" (hexenate (nacl.randomBytes 32)) "params" params} :format :json}))]
-        ; [:ok {jsonrpc 2.0, id 1, result {pong true, c 12}}]
-        (when (and (= code :ok) (response "result"))
-          (response "result")))))
-
 (defn dht-address [public-key-b58 salt]
   (let [sha1 (goog.crypt.Sha1.)]
     (.update sha1 (join-uint8arrays (b58/decode public-key-b58) (string-to-uint8array salt)))
@@ -47,10 +48,7 @@
 (defn magnet-link [infohash & [filename]]
   (str "magnet:?xt=urn:btih:" infohash (if filename (str "&dn=" filename))))
 
-(defn dht-compute-sig [keypair params]
-  (let [params-encoded (js/Bencode.encode (clj->js params))
-        sig-unit (.substring params-encoded 1 (- (.-length params-encoded) 1))]
-    (hexenate (nacl.sign.detached (string-to-uint8array sig-unit) (.-secretKey keypair)))))
+; --- key management --- ;
 
 (defn keys-from-secret [secret]
   (.fromSecretKey nacl.sign.keyPair (js/Uint8Array.from secret)))
@@ -60,42 +58,6 @@
 
 (defn pk-from-secret [secret]
   (js/Array.from (.-publicKey (keys-from-secret secret))))
-
-(defn fetch-known-nodes []
-  (go
-    (let [known-nodes (<! (<api :get "known-nodes.txt"))]
-      (when (= (first known-nodes) :ok)
-        (vec (remove #(= % "") (.split (second known-nodes) "\n")))))))
-
-(defn refresh-known-nodes [& [known-nodes callback]]
-  (go
-    ; if we have no known nodes load known-nodes.txt from the server
-    (let [known-nodes (if (= (count known-nodes) 0) (<! (fetch-known-nodes)) known-nodes)]
-      ; TODO: loop through subset of known nodes querying their peer list
-      (if callback (callback known-nodes))
-      known-nodes)))
-
-(defn make-account
-  "Creates a new identity by encrypting a seed with a password. Returns encrypted seed. Use (decrypt-account-keys encrypted-seed password) to obtain the key pair."
-  [password & [seed]]
-  ; pick random seed if not supplied
-  ; generate random salt
-  ; scrypt password + salt
-  ; generate random nonce
-  ; use result to nacl.secretbox seed
-  ; return salt + nonce + box
-  )
-
-(defn decrypt-account-keys
-  "Decrypts seed using password and returns account keys {publicKey: ..., secretKey: ...}."
-  [encrypted-seed password]
-  ; extract salt from seed
-  ; script password + salt to generate box key
-  ; extract nonce from seed
-  ; nacl.secretbox decrypt seed using key
-  ; use seed to generate nacl keypair
-  ; return keypair
-  )
 
 (defn keypair-human-readable [k]
   {:publicKey (b58/encode (.-publicKey k))
@@ -119,18 +81,82 @@
       (.slice 0 32)
       (nacl.sign.keyPair.fromSeed)))
 
+; --- crypto helpers --- ;
+
+(defn dht-compute-sig [keypair params]
+  (let [params-encoded (js/Bencode.encode (clj->js params))
+        sig-unit (.substring params-encoded 1 (- (.-length params-encoded) 1))]
+    (hexenate (nacl.sign.detached (string-to-uint8array sig-unit) (.-secretKey keypair)))))
+
+(defn with-signature [keypair params]
+  (let [params-encoded (js/Bencode.encode (clj->js params))
+        signature (hexenate (nacl.sign.detached (string-to-uint8array params-encoded) (.-secretKey keypair)))]
+    (assoc params :s signature)))
+
+; --- API helpers --- ;
+
+(defn <api [method uri & [opts]]
+  (let [c (chan)]
+    (apply ({:get GET :post POST} method) [uri (assoc opts :handler #(put! c [:ok %]) :error-handler #(put! c [:error %]))])
+    c))
+
+(defn <json-rpc [node keypair method params]
+  (go (let [signed-timestamped-params (with-signature keypair (with-timestamp (merge params {:k (public-key-b58-from-keypair keypair)})))
+            [code response] (<! (<api :post (str node "/bw/rpc") {:params {"jsonrpc" "2.0" "method" method "id" (hexenate (nacl.randomBytes 32)) "params" signed-timestamped-params} :format :json}))]
+        ; [:ok {jsonrpc 2.0, id 1, result {pong true, c 12}}]
+        (when (and (= code :ok) (response "result"))
+          (response "result")))))
+
+(defn fetch-known-nodes []
+  (go
+    (let [known-nodes (<! (<api :get "known-nodes.txt"))]
+      (when (= (first known-nodes) :ok)
+        (vec (remove #(= % "") (.split (second known-nodes) "\n")))))))
+
+(defn refresh-known-nodes [& [known-nodes callback]]
+  (go
+    ; if we have no known nodes load known-nodes.txt from the server
+    (let [known-nodes (if (= (count known-nodes) 0) (<! (fetch-known-nodes)) known-nodes)]
+      ; TODO: loop through subset of known nodes querying their peer list
+      (if callback (callback known-nodes))
+      known-nodes)))
+
+; --- API calls --- ;
+
+(defn make-account
+  "Creates a new identity by encrypting a seed with a password. Returns encrypted seed. Use (decrypt-account-keys encrypted-seed password) to obtain the key pair."
+  [password & [seed]]
+  ; pick random seed if not supplied
+  ; generate random salt
+  ; scrypt password + salt
+  ; generate random nonce
+  ; use result to nacl.secretbox seed
+  ; return salt + nonce + box
+  )
+
+(defn decrypt-account-keys
+  "Decrypts seed using password and returns account keys {publicKey: ..., secretKey: ...}."
+  [encrypted-seed password]
+  ; extract salt from seed
+  ; script password + salt to generate box key
+  ; extract nonce from seed
+  ; nacl.secretbox decrypt seed using key
+  ; use seed to generate nacl keypair
+  ; return keypair
+  )
+
 ; fetch account profile
-; (go (print (<! (profile-fetch "http://localhost:8923" (public-key-b58-from-keypair (keypair-from-seed-b58 "H33xgBQj5jTU6bKC5iw6B9docquvNpDeKoSSWkCpcU58"))))))
+; (go (print (<! (profile-fetch node keypair "7Q9he6fH1m6xAk5buSSPwK4Jjmute9FjF5TgidTZqiHM"))))
 (defn profile-fetch
   "Fetch account's profile data. Asyncrhonous."
-  [node public-key-base58 & [callback]]
+  [node keypair public-key-base58 & [callback]]
   (go
-    (let [result (<! (<json-rpc node "dht-get" {:addresshash (dht-address public-key-base58 profile-namespace)}))]
+    (let [result (<! (<json-rpc node keypair "dht-get" {:addresshash (dht-address public-key-base58 profile-namespace)}))]
       (if callback (apply callback result))
       result)))
 
 ; update account profile
-; (go (print (<! (profile-update "http://localhost:8923" (keypair-from-seed-b58 "H33xgBQj5jTU6bKC5iw6B9docquvNpDeKoSSWkCpcU58") {:name "Test face" :email "tester@test.com"}))))
+; (go (print (<! (profile-update node keypair {:name "Test face" :email "tester@test.com"}))))
 (defn profile-update
   "Update account's profile data. Asynchronous."
   [node keypair datastructure & [callback]]
@@ -143,18 +169,18 @@
         ["Profile data too large." nil]
         ; generate signed packet
         (let [; get previous value
-              [err response] (<! (<json-rpc node "dht-get" {:addresshash (dht-address public-key-base58 profile-namespace)}))
+              [err response] (<! (<json-rpc node keypair "dht-get" {:addresshash (dht-address public-key-base58 profile-namespace)}))
               dht-params {:v datastructure-bencoded :seq (if response (inc (response "seq")) 1) :salt profile-namespace}
               sig (dht-compute-sig keypair dht-params)
               post-data (merge dht-params {:k public-key-base58 :s.dht sig})
               ; post to nodes
-              response (<! (<json-rpc node "dht-put" post-data))
+              response (<! (<json-rpc node keypair "dht-put" post-data))
               [err dht-hash node-count] response]
           (if callback (callback err dht-hash node-count))
           response)))))
 
 ; fetch content
-; (go (let [c (content-fetch "http://45.32.253.186:8923" "448c6cc0e816408859ba8753705c2b2264548fdf")]
+; (go (let [c (content-fetch node keypair "448c6cc0e816408859ba8753705c2b2264548fdf")]
 ;      (loop []
 ;        (let [r (<! c)]
 ;          (print "got" r)
@@ -162,16 +188,16 @@
 ;            (recur))))))
 (defn content-fetch
   "Fetch some content by hash. Asynchronous."
-  [node infohash & [callback]]
+  [node keypair infohash & [callback]]
   (let [uid (hexenate (nacl.randomBytes 8))
         c (chan)]
     (print uid c)
-    (let [new-uid (<! (<json-rpc node "torrent-fetch" {:infohash infohash :u uid}))]
+    (let [new-uid (<! (<json-rpc node keypair "torrent-fetch" {:infohash infohash :u uid}))]
       (print new-uid)
       (if new-uid
         (print "new-uid" new-uid)
         (loop [after 0]
-          (let [update (<! (<json-rpc node "get-queue" {:u new-uid :after after}))
+          (let [update (<! (<json-rpc node keypair "get-queue" {:u new-uid :after after}))
                 latest-timestamp (apply js/Math.max (map #(get % "timestamp") update))]
             (print update)
             (for [u update]
@@ -184,16 +210,15 @@
     c))
 
 ; store content
-; (go (print (<! (content-store "http://localhost:8923" "7Q9he6fH1m6xAk5buSSPwK4Jjmute9FjF5TgidTZqiHM.json" (js/JSON.stringify (clj->js {:version "https://jsonfeed.org/version/1" :title "Testing" :items []}))))))
+; (go (print (<! (content-store node keypair "7Q9he6fH1m6xAk5buSSPwK4Jjmute9FjF5TgidTZqiHM.json" (js/JSON.stringify (clj->js {:version "https://jsonfeed.org/version/1" :title "Testing" :items []}))))))
 (defn content-store
   "Store some content. Returns the hash. Asynchronous."
-  [node content-name content & [callback]]
+  [node keypair content-name content & [callback]]
   (go
-    (let [[err infohash] (<! (<json-rpc node "torrent-seed" {:name content-name :content content}))]
+    (let [[err infohash] (<! (<json-rpc node keypair "torrent-seed" {:name content-name :content content}))]
       (if callback (callback err infohash))
       [err infohash])))
 
 ; get posts
-
 ; add post
 
